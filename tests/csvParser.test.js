7e477dc4c3a42116fa7ab243a4def4f27792508b
tepokato <tepokato@gmail.com>
2025-08-09 20:32:33 -0700

    Handle Windows line endings

--- tests/csvParser.test.js
+++ tests/csvParser.test.js
@@ -8,4 +8,9 @@ describe('parseCSVLine', () => {
   test('handles quoted commas', () => {
     expect(parseCSVLine('"123","Doe, John"')).toEqual(['123', 'Doe, John']);
   });
+
+  test('parses line ending with \r\n', () => {
+    const line = '123,John Doe\r\n'.split('\n')[0];
+    expect(parseCSVLine(line)).toEqual(['123', 'John Doe']);
+  });
 });
